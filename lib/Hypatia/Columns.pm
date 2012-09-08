824362ada0131f2db93941f08a7e78d2bf910491
Jack Maney <jack@jackmaney.com>
2012-09-08 01:31:39 +0000

    Import of JACKMANEY/Hypatia-0.012 from CPAN.
    gitpan-cpan-distribution: Hypatia
    gitpan-cpan-version:      0.012
    gitpan-cpan-path:         JACKMANEY/Hypatia-0.012.tar.gz
    gitpan-cpan-author:       JACKMANEY
    gitpan-cpan-maturity:     released

--- lib/Hypatia/Columns.pm
+++ lib/Hypatia/Columns.pm
@@ -1,6 +1,6 @@
 package Hypatia::Columns;
 {
-  $Hypatia::Columns::VERSION = '0.011';
+  $Hypatia::Columns::VERSION = '0.012';
 }
 use Moose;
 use Array::Utils qw(unique);
@@ -114,7 +114,7 @@ Hypatia::Columns
 
 =head1 VERSION
 
-version 0.011
+version 0.012
 
 =head1 AUTHOR
 
